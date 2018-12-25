1f5657bf67741249d12857ba56149897fb2c3919
rohit07cf <39081902+rohit07cf@users.noreply.github.com>
2018-12-25 11:11:35 +0530

    Update Flask_Sample.py

--- Flask_Sample.py
+++ Flask_Sample.py
@@ -1,31 +1,13 @@
-
-# coding: utf-8
-
-# In[ ]:
-
-
 #Flask Imports
 from flask import Flask
 from flask import jsonify
 from flask import request
 
-
-# In[ ]:
-
-
 app=Flask(__name__)
-
-
-# In[ ]:
-
 
 @app.route("/")
 def hello12():
     return "Hello World!"
-
-
-# In[ ]:
-
 
 @app.route('/getRequest', methods=['get'])
 def create_cm():
@@ -33,10 +15,6 @@ def create_cm():
     change = request.args.get('change', None)
     # do something, eg. return json response
     return jsonify({'summary': summary, 'change': change})
-
-
-# In[ ]:
-
 
 @app.route('/getPostParameters',methods=['POST'])
 def calculateModelAccuracyNew():
@@ -48,28 +26,16 @@ def calculateModelAccuracyNew():
     finalStr="Name:"+name+" Age:"+age+" Address:"+str(address)
     return finalStr;
 
-
-# In[ ]:
-
-
 def shutdown_server():
     func = request.environ.get('werkzeug.server.shutdown')
     if func is None:
         raise RuntimeError('Not running with the Werkzeug Server')
     func()
 
-
-# In[ ]:
-
-
 @app.route('/shutdown', methods=['POST'])
 def shutdown():
     shutdown_server()
     return 'Server shutting down...'
-
-
-# In[ ]:
-
 
 if __name__ == "__main__":
     app.run(app.run(host='0.0.0.0', port=8084))
